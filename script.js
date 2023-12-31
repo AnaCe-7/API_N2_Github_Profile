c60bfbe2ce1fd73e28f9ca228c17e3e6cd59207c
AnaCe-7 <ana.cecilia.reques@gmail.com>
2023-12-31 12:51:54 +0100

    Update card structure in script.js

--- script.js
+++ script.js
@@ -2,3 +2,38 @@ const APIURL = 'https://api.github.com/users/'
 const searchProfile = document.getElementById('search');
 const main = document.getElementById('main');
 
+function showProfile () {
+    axios.get(APIURL)
+        .then((response) => {
+            const profile = response.data
+
+            profile.forEach(profile => {
+
+                const card =
+                `<div class="card">
+                    <img class="avatar" src="${response.data.avatar_url}" alt="Pokemon avatar">
+                    <div class="user-info">
+                        <h2 class="name">${response.data.name}</h2>
+                        <p class="bio">${response.data.bio}</p>
+                        <ul>
+                            <li>${response.data.followers_url} Followers</li>
+                            <li>${response.data.following_url} Following</li>
+                            <li>${response.data.public_repos} Repos</li>        
+                        </ul>
+
+                        <ul>
+                            <li>${response.data.repos_url.name}</li>      
+                        </ul>
+                    </div>
+                </div>`
+
+                main.innerHTML = card;
+            });
+
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+};
+
+
